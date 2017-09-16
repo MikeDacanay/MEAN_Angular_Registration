6592c7637d349fcd3e7bc53bb32fbe45dc7fa372
Mike Dacanay <migd64@gmail.com>
2017-09-15 18:09:06 -0700

    work on register button

--- Registration/src/app/app.component.ts
+++ Registration/src/app/app.component.ts
@@ -6,11 +6,12 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	title: String = 'Registration';
+	title: string = 'Registration';
 
 	user = {
+		lastName:'',
 	}
-	OnSubmit(){
-		console.log('hello');
+
+	onSubmit(){
 	}
 }
