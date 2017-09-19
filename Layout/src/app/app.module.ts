52c4233ff3bade4040cf63e9cd218cd3758d9c7a
Mike Dacanay <migd64@gmail.com>
2017-09-18 18:12:35 -0700

    final commit, layout in prompt imitated

--- Layout/src/app/app.module.ts
+++ Layout/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
 import { NavComponnentComponent } from './nav-componnent/nav-componnent.component';
+import { MainComponentComponent } from './main-component/main-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponentComponent,
-    NavComponnentComponent
+    NavComponnentComponent,
+    MainComponentComponent
   ],
   imports: [
     BrowserModule
